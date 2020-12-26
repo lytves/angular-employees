50f79139c467a2b1356ea8a5891aeb3e79423c6f
lytves <22356770+lytves@users.noreply.github.com>
2020-12-26 19:46:18 +0100

    Bootstrap imported, app.component updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ListEmployeesComponent} from './components/list-employees/list-employees.component';
+import {CreateEmployeeComponent} from './components/create-employee/create-employee.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    NavbarComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule
